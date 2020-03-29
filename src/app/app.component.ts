34ac0813d8a21c613a89699ce0d93b2da866acb9
Edward Lim <Edward.Lim@redcypress.com.tw>
2020-03-29 19:36:12 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'briuin-experience-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'briuin-experience';
+}
